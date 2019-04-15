7185bf9e1604b40e1e6b774744ec63e022a60fd3
Théophile de Cagny <theophile.decagny@epitech.eu>
2019-04-15 12:23:46 +0100

    keras neural

--- url_classifier.py
+++ url_classifier.py
@@ -1,6 +1,12 @@
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(1)
+
+from keras import layers
+#from keras import regularizers
+from keras.models import Sequential
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import GaussianNB
 
 import pandas as pb
 import numpy as np
@@ -15,11 +21,22 @@ def parse_training_file(path='data/training.csv', label_row='phishing'):
 
     return label_values, feature_names, feature_values
 
+def create_baseline():
+    model = Sequential()
+
+    model.add(layers.Dense(5, activation='tanh'))
+    model.add(layers.Dropout(0.6))
+    model.add(layers.Dense(5, activation='tanh'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 def main():
     print_header()
     label_values, feature_names, feature_values = parse_training_file()
-    test_size = 0.33
+    test_size = 0.25
 
     print_parameters(feature_names, feature_values, test_size)
 
@@ -28,13 +45,28 @@ def main():
                                                                             test_size=test_size,
                                                                             random_state=42)
 
-    gnb = GaussianNB()
-    gnb.fit(train_values, train_labels)
-    predictions = gnb.predict(test_values)
+    # gnb = GaussianNB()
+    # gnb.fit(train_values, train_labels)
+    # predictions = gnb.predict(test_values)
 
+    nb_iterations = 8
+
+    # evaluate model with standardized dataset
+    model = create_baseline()
+    model.compile(optimizer='adagrad',loss='mse',metrics=['accuracy'])
+    #model.fit(train_values,train_labels,epochs=nb_iterations)
+
+    predictions = []
+    probas = model.predict(test_values)
+    for prediction in probas:
+        if (prediction >= 0.5):
+            predictions.append(0)
+        else:
+            predictions.append(1)
     label_counts = np.unique(test_labels, return_counts=True)
     pred_counts = np.unique(predictions, return_counts=True)
 
+    print("lab")
     print_output(label_counts, pred_counts, predictions, test_labels)
 
 
@@ -62,13 +94,17 @@ def print_parameters(feature_names, feature_values, test_size):
 
 def print_output(label_counts, pred_counts, predictions, test_labels):
     print("\n-----------------------------------------------------------\nresults:\n")
+    print("label_counts :", label_counts)
+    print("pred_counts :", pred_counts)
     print(">> number of benign urls: ")
     print("\t| real      :\t", label_counts[1][0])
     print("\t| predicted :\t", pred_counts[1][0], '\n')
     print(">> number of phishing urls: ")
     print("\t| real      :\t", label_counts[1][1])
     print("\t| predicted :\t", pred_counts[1][1], '\n')
-    print(">> the accuracy score is:", str(round(accuracy_score(test_labels, predictions) * 100, 2)) + '%\n')
+    total = label_counts[1][0] + label_counts[1][1]
+    print(">> the accuracy score is:", str(100 - round((abs(label_counts[1][0] - pred_counts[1][0]) + (abs(label_counts[1][1] - pred_counts[1][1])) * 100) / total,
+                                                 2)) + '%\n')
 
 
 if __name__ == "__main__":
